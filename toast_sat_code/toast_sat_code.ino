3e687eb06d96248c1d2b243842b9981dda9f5232
Hamish Hare <22hvh1@queensu.ca>
2023-02-25 18:04:11 -0500

    Not touched up or checked, but this is the first potentially finished version of the code. I completed the storage to file part of the loop and implemented stop conditions.

--- toast_sat_code/toast_sat_code.ino
+++ toast_sat_code/toast_sat_code.ino
@@ -16,7 +16,11 @@ SFE_BMP180 pressure;
 File dataFile;
 File logFile;
 
-bool serialOutput = false;
+const unsigned long MIN_TIME_MS = 3600000; // The minimum time to run the code for in milliseconds (one hour)
+const unsigned long MAX_TIME_MS = 18000000; // The maximum time to run the code for in milliseconds (five hours)
+const float MIN_HEIGHT = 300.0; // The minimum height in metres, below which we stop recording data
+const unsigned int MIN_NUM_MEASUREMENTS = 300; // The minimum number of measurements
+const bool serialOutput = true;
 
 void setup() {
   if (serialOutput){ // For interfacing with Serial monitor
@@ -66,24 +70,27 @@ void setup() {
     //=======================
     // Initialise the SD Card (See https://www.arduino.cc/reference/en/libraries/sd/)
     // (Don't run the rest of the code if this fails)
-    Serial.begin(9600);
+    //Serial.begin(9600);
     if (!SD.begin(10)) { // uses the default SS hardware pin of 10
       while (1);
     }
     // Save to a log file that the SD card was succesfully initialised
     logFile = SD.open("logs.txt", FILE_WRITE);
-    logFile.println("SD card successfully initialised at [TIME]"); // =========== ADD TIME IDENTIFIER
+    logFile.print("SD card successfully initialised at ");
+    logFile.println(millis());
     //Serial.println("Log file opened");
     
     // Initialise the Adafruit Vis/IR/UV sensor
     if (uv.begin()) {
       // Save to a log file that the light sensor was succesfully initialised
-      logFile.println("Light sensor successfully initialised at [TIME]"); // =========== ADD TIME IDENTIFIER
+      logFile.print("Light sensor successfully initialised at ");
+      logFile.println(millis());
       //Serial.println("Light sensor started");
     }
     else{
       // Save to a log file that the light sensor failed to initialise
-      logFile.println("Light sensor failed to initialise at [TIME]"); // =========== ADD TIME IDENTIFIER
+      logFile.print("Light sensor failed to initialise at ");
+      logFile.println(millis());
       //Serial.println("Light sensor failed");
       logFile.close();
       while (1); // pause forever
@@ -92,12 +99,14 @@ void setup() {
     // Initialize the pressure sensor (it is important to get calibration values stored on the device).      
     if (pressure.begin()){
       // Save to a log file that the pressure sensor was succesfully initialised
-      logFile.println("Pressure sensor successfully initialised at [TIME]"); // =========== ADD TIME IDENTIFIER
+      logFile.print("Pressure sensor successfully initialised at ");
+      logFile.println(millis());
       //Serial.println("Pressure sensor started");
     } else
     {
       // Save to a log file that the pressure sensor failed to initialise
-      logFile.println("Pressure sensor failed to initialise at [TIME]"); // =========== ADD TIME IDENTIFIER
+      logFile.print("Pressure sensor failed to initialise at ");
+      logFile.println(millis());
       //Serial.println("Pressure sensor failed");
       logFile.close(); 
       while(1); // Pause forever.
@@ -109,7 +118,7 @@ void setup() {
     // OPEN THE DATA FILE
     // note that only one file can be open at a time
     dataFile = SD.open("flightData.txt", FILE_WRITE);
-    dataFile.println("Time");
+    dataFile.println("Time (ms), Visible (arb.), IR (arb.), UV Index, Temperature (deg C), Pressure (mbar), Altitude (m)");
   }
 }
 
@@ -117,16 +126,17 @@ void setup() {
 // For pressure sensor
 char status;
 double T,P,p0,a;
+unsigned int num_measurements = 0;
 
 void loop() {
-  if serialOutput{
+  if (serialOutput){
     // Adafruit sensor:
     Serial.println("ADAFRUIT VIS/IR/UV:");
     Serial.println("===================");
     Serial.print("Vis: "); Serial.println(uv.readVisible());
-    Serial.print(" (arb. units)");
+    Serial.println(" (arb. units)");
     Serial.print("IR: "); Serial.println(uv.readIR());
-    Serial.print(" (arb. units)");
+    Serial.println(" (arb. units)");
     
     // Uncomment if you have an IR LED attached to LED pin!
     //Serial.print("Prox: "); Serial.println(uv.readProx());
@@ -167,13 +177,16 @@ void loop() {
     if (status != 0)
     {
       // Wait for the measurement to complete:
+      Serial.print("Time before delay: "); Serial.println(millis()); // ===================== DELETE, JUST FOR TESTING
       delay(status);
+      Serial.print("Time after delay: "); Serial.println(millis()); // ===================== DELETE, JUST FOR TESTING
 
       // Retrieve the completed temperature measurement:
       // Note that the measurement is stored in the variable T.
       // Function returns 1 if successful, 0 if failure.
 
       status = pressure.getTemperature(T);
+
       if (status != 0)
       {
         // Print out the measurement:
@@ -235,6 +248,99 @@ void loop() {
     }
     else Serial.println("error starting temperature measurement\n");
   }
+  else{ // If saving to the file
+    // PERFORM THE CODE
+    dataFile.print(millis()); dataFile.print(","); // time
+
+    // LIGHT DATA
+    dataFile.print(uv.readVisible()); dataFile.print(","); // visible
+    dataFile.print(uv.readIR()); dataFile.print(","); // infrared
+    dataFile.print(uv.readUV()/100); dataFile.print(","); // UV index
+    delay(500); // a delay of 500 ms to ensure all data is captured
+
+    // PRESSURE DATA
+    // Start a temperature measurement:
+    status = pressure.startTemperature();
+    if (status != 0)
+    {
+      // Wait for the measurement to complete:
+      delay(status);
+      // Retrieve the completed temperature measurement:
+      // Note that the measurement is stored in the variable T.
+      // Function returns 1 if successful, 0 if failure.
+      status = pressure.getTemperature(T);
+
+      if (status != 0)
+      {
+        // Print out the temperature measurement:
+        dataFile.print(T); dataFile.print(","); // === MAY WANT TO LIMIT THIS TO E.G. 2DP WITH print(T, 2).
+        
+        // Start a pressure measurement:
+        // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
+        // If request is successful, the number of ms to wait is returned.
+        // If request is unsuccessful, 0 is returned.
+        status = pressure.startPressure(3);
+        if (status != 0)
+        {
+          // Wait for the measurement to complete:
+          delay(status);
+
+          // Retrieve the completed pressure measurement:
+          // Note that the measurement is stored in the variable P.
+          // Note also that the function requires the previous temperature measurement (T).
+          // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
+          // Function returns 1 if successful, 0 if failure.
+          status = pressure.getPressure(P,T);
+          if (status != 0)
+          {
+            // Print out the measurement:
+            // absolute pressure
+            dataFile.print(P); dataFile.print(","); // === MAY WANT TO LIMIT THIS TO E.G. 2DP WITH print(P, 2).
+
+            // The pressure sensor returns absolute pressure, which varies with altitude.
+            // To remove the effects of altitude, use the sealevel function and your current altitude.
+            // This number is commonly used in weather reports.
+            // Parameters: P = absolute pressure in mb, ALTITUDE = current altitude in m.
+            // Result: p0 = sea-level compensated pressure in mb
+
+            p0 = pressure.sealevel(P,ALTITUDE); // computed for the provided altitude at the top, we used 100
+            // dataFile.print(p0); dataFile.print(","); // ==== IF WE WANT TO SAVE THE PRESSURE RELATIVE TO SEA-LEVEL, UNCOMMENT THIS
+
+            // On the other hand, if you want to determine your altitude from the pressure reading,
+            // use the altitude function along with a baseline pressure (sea-level or other).
+            // Parameters: P = absolute pressure in mb, p0 = baseline pressure in mb.
+            // Result: a = altitude in m.
+
+            // computed altitude
+            a = pressure.altitude(P,p0); 
+            dataFile.print(a); dataFile.print(","); // === MAY WANT TO LIMIT THIS TO E.G. NO D.P.s WITH print(a, 0).
+          }
+          else dataFile.print("[error retrieving pressure measurement],");
+        }
+        else dataFile.print("[error starting pressure measurement],");
+      }
+      else dataFile.print("[error retrieving temperature measurement],");
+    }
+    else dataFile.print("[error starting temperature measurement],");
+
+    // Start a new line in the data file and increase number of measurements counter
+    dataFile.print("\n");
+    num_measurements++;
+  }
   
+  // WAIT BEFORE NEXT MEASUREMENT
   delay(5000);  // Pause for 5 seconds.
+
+  // EXIT CONDITION
+  // Stop if a minimum amount of time has passed and the satellite is below a minimum height
+  // with a minimum number of measurements taken...
+  // ... or if we are over the maximum amount of time.
+  // (START AN INFINITE WHILE LOOP WHEN MET)
+  if ((millis() > MIN_TIME_MS && a < MIN_HEIGHT && num_measurements > MIN_NUM_MEASUREMENTS) || millis() > MAX_TIME_MS){
+    if (!serialOutput){
+      // Close the data file.
+      dataFile.close();
+    }
+    while(1);
+  }
 }
