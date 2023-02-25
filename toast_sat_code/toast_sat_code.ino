3c692a723a0afa2c1028eac28ed4196ce3983cc2
Hamish Hare <22hvh1@queensu.ca>
2023-02-25 16:52:28 -0500

    A bit of preliminary work on the loop.

--- toast_sat_code/toast_sat_code.ino
+++ toast_sat_code/toast_sat_code.ino
@@ -109,6 +109,7 @@ void setup() {
     // OPEN THE DATA FILE
     // note that only one file can be open at a time
     dataFile = SD.open("flightData.txt", FILE_WRITE);
+    dataFile.println("Time");
   }
 }
 
@@ -118,121 +119,122 @@ char status;
 double T,P,p0,a;
 
 void loop() {
+  if serialOutput{
+    // Adafruit sensor:
+    Serial.println("ADAFRUIT VIS/IR/UV:");
+    Serial.println("===================");
+    Serial.print("Vis: "); Serial.println(uv.readVisible());
+    Serial.print(" (arb. units)");
+    Serial.print("IR: "); Serial.println(uv.readIR());
+    Serial.print(" (arb. units)");
+    
+    // Uncomment if you have an IR LED attached to LED pin!
+    //Serial.print("Prox: "); Serial.println(uv.readProx());
 
-  // Adafruit sensor:
-  Serial.println("ADAFRUIT VIS/IR/UV:");
-  Serial.println("===================");
-  Serial.print("Vis: "); Serial.println(uv.readVisible());
-  Serial.print(" (arb. units)");
-  Serial.print("IR: "); Serial.println(uv.readIR());
-  Serial.print(" (arb. units)");
-  
-  // Uncomment if you have an IR LED attached to LED pin!
-  //Serial.print("Prox: "); Serial.println(uv.readProx());
+    //float UVindex = uv.readUV(); ========== I COMMENTED THIS OUT AS DIDN'T LIKE IT REDIFINING THE VARIABLE EACH LOOP, BUT KEEPING AS REFERENCE
+    // the index is multiplied by 100 so to get the
+    // integer index, divide by 100!
+    //UVindex /= 100.0;  
+    //Serial.print("UV: ");  Serial.println(UVindex);
+    Serial.print("UV Index: ");  Serial.println(uv.readUV()/100);
 
-  //float UVindex = uv.readUV(); ========== I COMMENTED THIS OUT AS DIDN'T LIKE IT REDIFINING THE VARIABLE EACH LOOP, BUT KEEPING AS REFERENCE
-  // the index is multiplied by 100 so to get the
-  // integer index, divide by 100!
-  //UVindex /= 100.0;  
-  //Serial.print("UV: ");  Serial.println(UVindex);
-  Serial.print("UV Index: ");  Serial.println(uv.readUV()/100);
+    delay(1000); // =========== MAY NOT BE NEEDED? DEPENDS HOW LONG READING TAKES
 
-  delay(1000); // =========== MAY NOT BE NEEDED? DEPENDS HOW LONG READING TAKES
+    // Pressure sensor:
 
-  // Pressure sensor:
+    Serial.println("PRESSURE/TEMP SENSOR:");
+    Serial.println("=====================");
+    // Loop here getting pressure readings.
 
-  Serial.println("PRESSURE/TEMP SENSOR:");
-  Serial.println("=====================");
-  // Loop here getting pressure readings.
+    // If you want sea-level-compensated pressure, as used in weather reports,
+    // you will need to know the altitude at which your measurements are taken.
+    // We're using a constant called ALTITUDE in this sketch:
+    
+    Serial.print("provided altitude: ");
+    Serial.print(ALTITUDE,0);
+    Serial.println(" meters");
+    
+    // If you want to measure altitude, and not pressure, you will instead need
+    // to provide a known baseline pressure. This is shown at the end of the sketch.
 
-  // If you want sea-level-compensated pressure, as used in weather reports,
-  // you will need to know the altitude at which your measurements are taken.
-  // We're using a constant called ALTITUDE in this sketch:
-  
-  Serial.print("provided altitude: ");
-  Serial.print(ALTITUDE,0);
-  Serial.println(" meters");
-  
-  // If you want to measure altitude, and not pressure, you will instead need
-  // to provide a known baseline pressure. This is shown at the end of the sketch.
+    // You must first get a temperature measurement to perform a pressure reading.
+    
+    // Start a temperature measurement:
+    // If request is successful, the number of ms to wait is returned.
+    // If request is unsuccessful, 0 is returned.
 
-  // You must first get a temperature measurement to perform a pressure reading.
-  
-  // Start a temperature measurement:
-  // If request is successful, the number of ms to wait is returned.
-  // If request is unsuccessful, 0 is returned.
-
-  status = pressure.startTemperature();
-  if (status != 0)
-  {
-    // Wait for the measurement to complete:
-    delay(status);
-
-    // Retrieve the completed temperature measurement:
-    // Note that the measurement is stored in the variable T.
-    // Function returns 1 if successful, 0 if failure.
-
-    status = pressure.getTemperature(T);
+    status = pressure.startTemperature();
     if (status != 0)
     {
-      // Print out the measurement:
-      Serial.print("temperature: ");
-      Serial.print(T,2);
-      Serial.println(" deg C");
-      
-      // Start a pressure measurement:
-      // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
-      // If request is successful, the number of ms to wait is returned.
-      // If request is unsuccessful, 0 is returned.
+      // Wait for the measurement to complete:
+      delay(status);
 
-      status = pressure.startPressure(3);
+      // Retrieve the completed temperature measurement:
+      // Note that the measurement is stored in the variable T.
+      // Function returns 1 if successful, 0 if failure.
+
+      status = pressure.getTemperature(T);
       if (status != 0)
       {
-        // Wait for the measurement to complete:
-        delay(status);
+        // Print out the measurement:
+        Serial.print("temperature: ");
+        Serial.print(T,2);
+        Serial.println(" deg C");
+        
+        // Start a pressure measurement:
+        // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
+        // If request is successful, the number of ms to wait is returned.
+        // If request is unsuccessful, 0 is returned.
 
-        // Retrieve the completed pressure measurement:
-        // Note that the measurement is stored in the variable P.
-        // Note also that the function requires the previous temperature measurement (T).
-        // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
-        // Function returns 1 if successful, 0 if failure.
-
-        status = pressure.getPressure(P,T);
+        status = pressure.startPressure(3);
         if (status != 0)
         {
-          // Print out the measurement:
-          Serial.print("absolute pressure: ");
-          Serial.print(P,2);
-          Serial.println(" mb");
+          // Wait for the measurement to complete:
+          delay(status);
 
-          // The pressure sensor returns absolute pressure, which varies with altitude.
-          // To remove the effects of altitude, use the sealevel function and your current altitude.
-          // This number is commonly used in weather reports.
-          // Parameters: P = absolute pressure in mb, ALTITUDE = current altitude in m.
-          // Result: p0 = sea-level compensated pressure in mb
+          // Retrieve the completed pressure measurement:
+          // Note that the measurement is stored in the variable P.
+          // Note also that the function requires the previous temperature measurement (T).
+          // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
+          // Function returns 1 if successful, 0 if failure.
 
-          p0 = pressure.sealevel(P,ALTITUDE); // we're at 1655 meters (Boulder, CO)
-          Serial.print("relative (sea-level) pressure: ");
-          Serial.print(p0,2);
-          Serial.println(" mb");
+          status = pressure.getPressure(P,T);
+          if (status != 0)
+          {
+            // Print out the measurement:
+            Serial.print("absolute pressure: ");
+            Serial.print(P,2);
+            Serial.println(" mb");
 
-          // On the other hand, if you want to determine your altitude from the pressure reading,
-          // use the altitude function along with a baseline pressure (sea-level or other).
-          // Parameters: P = absolute pressure in mb, p0 = baseline pressure in mb.
-          // Result: a = altitude in m.
+            // The pressure sensor returns absolute pressure, which varies with altitude.
+            // To remove the effects of altitude, use the sealevel function and your current altitude.
+            // This number is commonly used in weather reports.
+            // Parameters: P = absolute pressure in mb, ALTITUDE = current altitude in m.
+            // Result: p0 = sea-level compensated pressure in mb
 
-          a = pressure.altitude(P,p0);
-          Serial.print("computed altitude: ");
-          Serial.print(a,0);
-          Serial.println(" meters");
+            p0 = pressure.sealevel(P,ALTITUDE); // we're at 1655 meters (Boulder, CO)
+            Serial.print("relative (sea-level) pressure: ");
+            Serial.print(p0,2);
+            Serial.println(" mb");
+
+            // On the other hand, if you want to determine your altitude from the pressure reading,
+            // use the altitude function along with a baseline pressure (sea-level or other).
+            // Parameters: P = absolute pressure in mb, p0 = baseline pressure in mb.
+            // Result: a = altitude in m.
+
+            a = pressure.altitude(P,p0);
+            Serial.print("computed altitude: ");
+            Serial.print(a,0);
+            Serial.println(" meters");
+          }
+          else Serial.println("error retrieving pressure measurement\n");
         }
-        else Serial.println("error retrieving pressure measurement\n");
+        else Serial.println("error starting pressure measurement\n");
       }
-      else Serial.println("error starting pressure measurement\n");
+      else Serial.println("error retrieving temperature measurement\n");
     }
-    else Serial.println("error retrieving temperature measurement\n");
+    else Serial.println("error starting temperature measurement\n");
   }
-  else Serial.println("error starting temperature measurement\n");
-
+  
   delay(5000);  // Pause for 5 seconds.
 }
